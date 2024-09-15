5caf9087aebf9b9e5c2f343e1bb019105fb30025
Sycinz <gronbosof@gmail.com>
2024-09-15 16:05:32 +0200

    Header component created and assets added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import './scss/App.scss'
+import Header from './components/Header'
 
 function App() {
   return (
     <>
-      Initial commit
+      <Header />
     </>
   )
 }
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="header_logo">Falo<span className="upper_index">DTL</span></div>
+        </header>
+    )
+}
+
+export default Header
