c340319c54eba154737c7f5d1e98d9fb272ae87e
Sycinz <gronbosof@gmail.com>
2024-11-02 11:41:23 +0100

    Making everything new

--- client/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-const Header = () => {
-    return (
-        <header className="header">
-            <div className="header_logo">Falo<span className="upper_index">DTL</span></div>
-        </header>
-    )
-}
-
-export default Header
